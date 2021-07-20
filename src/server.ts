aa6803d75cb9090a765e9a7839fb8f7861ea95d8
yamilaBacchini <y.bacchini@hotmail.com>
2021-07-20 14:55:49 -0300

    feat: added cors support and configs

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,61 @@
 import * as express from 'express';
-import {createConnection, getManager} from 'typeorm';
-import {AdoptionCatRepository} from "./repository/AdoptionCat-Repository";
-import {AdoptionCat} from "./entity/AdoptionCat";
+import {createConnection} from 'typeorm';
 import {AdoptionCatService} from "./service/AdoptionCat-Service";
+// import {AdoptionCatRepository} from "./repository/AdoptionCat-Repository";
+import * as cors from 'cors';
+
 
 createConnection().then(connection => {
     connection.synchronize();
 }).catch(error => console.log(error));
 
-const adoptionCatRepository = new AdoptionCatRepository();
+// const adoptionCatRepository = new AdoptionCatRepository();
 const adoptionCatService = new AdoptionCatService();
 
 const app = express();
+
+var router = express.Router();
+
+const options: cors.CorsOptions = {
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'X-Access-Token',
+    ],
+    credentials: true,
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    origin: '*',
+    preflightContinue: false,
+  };
+
+  router.use(cors(options));
+
+//add your routes
+
+//enable pre-flight
+router.options('*', cors(options));
+
 app.use(express.json());
 
-app.get('/getAllAdoptionCats', (req, res) => {
+router.get('/getAllAdoptionCats', (req, res) => {
     adoptionCatService.getAll().then((resultado) => res.send(resultado));
 });
 
-app.get('/getAdoptionCatById/:id', (req, res) => {
+router.get('/getAdoptionCatById/:id', (req, res) => {
     adoptionCatService.getById(Number(req.params.id)).then((resultado) => res.send(resultado));
 })
 
-app.get('/getAdoptionCatsByFilters', (req, res) => {
+router.get('/getAdoptionCatsByFilters', (req, res) => {
     adoptionCatService.getByFilters(req.body).then((resultado) => res.send(resultado));
 })
 
-app.post('/createAdoptionCat', (req, res) => {
+router.post('/createAdoptionCat', (req, res) => {
     adoptionCatService.create(req.body).then((resultado) => res.send(resultado));
 })
+
+app.use(router);
 
 app.listen(3000, () => {
     console.log('Ready on port 3000!');
